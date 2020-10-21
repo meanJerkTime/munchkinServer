428984646b289d611a1c1ad7e738b89359f38bde
BrendonLH <Brendonhampton1988@gmail.com>
2020-10-21 13:41:12 -0700

    back to 3 repos

--- index.js
+++ index.js
@@ -26,4 +26,3 @@ connection.connect(function(err) {
 });
 
 server.start(process.env.PORT);
-// server.start(process.env.PORT);
--- server.js
+++ server.js
@@ -1,50 +1,12 @@
 'use strict';
-const PORT = process.env.PORT || 3000;
+
 const express = require('express');
 const app = express();
-// const http = require('http');
-// const server = http.createServer(app);
-
-
-const server = express()
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
-
-
-const io = require('socket.io').listen(server);
 
 app.use(express.urlencoded({extended:true}));
 
-let custom_id = 0;
-io.engine.generateId = (req) => {
-  return "User:" + custom_id + Math.floor(Math.random() * 10000);
-};
-
-io.on('connection', (socket) => {
-  
-  socket.on('fromClient', () => {
-    console.log(socket.id, 'Connected');
-      socket.emit('toClient');
-  });
-  socket.on('answers', (payload) => {
-    // console.log(payload.roomsList);
-  });
-
-  // on create room also joins the room
-  socket.on('createRoom', function(room) {
-    socket.join(room);
-    console.log(socket.id, 'joined Room', room);
-  });
-  // joins the room
-  socket.on('joinRoom', function(room) {
-    socket.join(room);
-    // console.log(socket.id, 'joined Room', room);
-  });
-  console.log(socket.adapter.rooms);
-});
- 
-
 function start(port) {
-    server.listen(port, () => console.log('server up on port', port)); 
+    app.listen(port, () => console.log('server up on port', port)); 
 }
 
 module.exports = {
