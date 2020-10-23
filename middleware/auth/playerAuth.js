a2d20bd9d79be8b6d1f99574b0d6a5dc3bc6c341
BrendonLH <Brendonhampton1988@gmail.com>
2020-10-23 11:56:33 -0700

    updated server with proper response to the hub

--- middleware/auth/playerAuth.js
+++ middleware/auth/playerAuth.js
@@ -5,10 +5,10 @@ const players = require('../models/players-model.js');
 
 module.exports = async(request,response,next) => {
     try {
-        let userRecord = await players.validateBasic(request.body.username,request.body.password);
-        console.log(userRecord);
+        let userRecord = await players.validateBasic(request.body.username,request.body.password); 
         if(request.user = userRecord) {
             next();
+
         }
     
     } catch (e) {
--- middleware/router.js
+++ middleware/router.js
@@ -1,16 +1,18 @@
 'use strict';
+const { request, response } = require('express');
 const express = require('express');
-const basicAuth = require('../middleware/auth/playerAuth.js');
+const playerAuth = require('../middleware/auth/playerAuth.js');
 const signUp = require('./playerSignUp.js');
 const router = express.Router();
 
 router.post ('/signup',signUp, async (request, response, next) => {
 });
 
-router.post('/signin',basicAuth, async (request,response,next) => {
+router.post('/signin',playerAuth, async (request,response,next) => {
     let showUser = {
         user:request.user.username
     }
+    console.log(showUser.user, 'signed in');
     response.status(200).send(showUser);
 });
 
